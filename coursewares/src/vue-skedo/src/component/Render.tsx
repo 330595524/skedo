8cc94dc0141eec8b39645e8bc4e0112f17ad918f
徐历敏 <xulimin@zuoyebang.com>
2022-03-29 08:38:14 +0800

    no message

--- coursewares/src/vue-skedo/src/component/Render.tsx
+++ coursewares/src/vue-skedo/src/component/Render.tsx
@@ -41,6 +41,8 @@ function renderItem(node: Node) {
       return (
         <div
           style={{
+            width: '50px',
+            height:'50px',
             backgroundColor: "yellow",
           }}
         />
